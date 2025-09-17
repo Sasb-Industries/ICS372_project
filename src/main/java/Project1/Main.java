c8b425f9e278bbdeea9b59f32160a5cc84c4abb0
Theophilus Cox III <theocoxiii@gmail.com>
2025-09-17 12:19:21 -0500

    changed some formatting and worked on the UI on main and got the restaurant class implemented.

--- src/main/java/Project1/Main.java
+++ src/main/java/Project1/Main.java
@@ -6,41 +6,53 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         OrderWrapper orderIn = Serialization.readOrder();
-
+        Resturant resturant = new Resturant(orderIn);
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("What would you like to do? \n 1: Start order\n");
+        System.out.println("What would you like to do? \n1: Start order");
         System.out.print("2: Complete Order \n3: Display an Order");
-        System.out.println("4: Display Incomplete Orders \n5: Print All Orders");
+        System.out.println("\n4: Display Incomplete Orders \n5: Print All Orders");
         System.out.println("6: Close up Shop");
         int choice = scan.nextInt();
-        if (choice == 1) {
-            System.out.println("Enter Order ID: ");
-            int scanID = scan.nextInt();
-            if (scanID == orderIn.getOrder().getOrderId() && orderIn.getOrder().getOrderStatus() == 1) {
-                orderIn.getOrder().setOrderStatus(2);
-                System.out.println("Order ID: " + orderIn.getOrder().getOrderId() + " is starting.");
-            } else {
-                System.out.println("Order ID: " + orderIn.getOrder().getOrderId() + " is already started or completed.");
+        while (choice !=6 ) {
+            if (choice == 1) {
+                resturant.startOrder();
+                System.out.println("What would you like to do? \n1: Start order");
+                System.out.print("2: Complete Order \n3: Display an Order");
+                System.out.println("\n4: Display Incomplete Orders \n5: Print All Orders");
+                System.out.println("6: Close up Shop");
+                choice = scan.nextInt();
+            }
+            else if (choice == 2) {
+                resturant.completeOrder();
+                System.out.println("What would you like to do? \n1: Start order");
+                System.out.print("2: Complete Order \n3: Display an Order");
+                System.out.println("\n4: Display Incomplete Orders \n5: Print All Orders");
+                System.out.println("6: Close up Shop");
+                choice = scan.nextInt();
+            }
+            else if (choice == 3) {
+                resturant.display();
+                System.out.println("What would you like to do? \n1: Start order");
+                System.out.print("2: Complete Order \n3: Display an Order");
+                System.out.println("\n4: Display Incomplete Orders \n5: Print All Orders");
+                System.out.println("6: Close up Shop");
+                choice = scan.nextInt();
+            }
+            else if (choice == 4) {
+
+            }
+            else if (choice == 5) {
+
             }
 
-        } else if (choice == 2) {
-            System.out.println("Enter Order ID: ");
-            int scanID = scan.nextInt();
-            if (scanID == orderIn.getOrder().getOrderId() && orderIn.getOrder().getOrderStatus() == 2) {
-                orderIn.getOrder().setOrderStatus(3);
-                System.out.println("Order ID: " + orderIn.getOrder().getOrderId() + " is completed.");
+            else{
+                System.out.println("Invalid choice. Try again.");
             }
-        } else if (choice == 3) {
-
-            for (Item i : orderIn.getOrder().getItems()) {
-                System.out.println(i.getName() + " - " + i.getPrice());
-            }
-        } else if (choice == 4) {
-
-        } else if (choice == 5) {
-
-        } else {
+        }
+        if (choice == 6) {
+            System.out.println("Have a great night, goodbye!");
+            scan.close();
         }
     }
 }
--- src/main/java/Project1/Resturant.java
+++ src/main/java/Project1/Resturant.java
@@ -1,15 +1,38 @@
 package Project1;
 
+import java.util.Scanner;
+
 public class Resturant {
-
-    public void display(){
-
+    private OrderWrapper order;
+    public Resturant(OrderWrapper order) {
+        this.order = order;
     }
-    public void startOrder(){
+    Scanner scan = new Scanner(System.in);
 
+    public void display() {
+        for (Item i : order.getOrder().getItems()) {
+            System.out.println(i.getName() + " - " + i.getQuantity());
+        }
     }
-    public void completeOrder(){
-
+    public void startOrder() {
+        System.out.println("Enter Order ID: ");
+        int scanID = scan.nextInt();
+        if (scanID == order.getOrder().getOrderId() && order.getOrder().getOrderStatus() == 1) {
+            order.getOrder().setOrderStatus(2);
+            System.out.println("Order ID: " + order.getOrder().getOrderId() + " is starting.");
+        } else {
+            System.out.println("Order ID: " + order.getOrder().getOrderId() + " is already started or completed.");
+        }
     }
-
+    public void completeOrder() {
+        System.out.println("Enter Order ID: ");
+        int scanID = scan.nextInt();
+        if (scanID == order.getOrder().getOrderId() && order.getOrder().getOrderStatus() == 2) {
+            order.getOrder().setOrderStatus(3);
+            System.out.println("Order ID: " + order.getOrder().getOrderId() + " is completed.");
+        }
+        else {
+            System.out.println("Order ID: " + order.getOrder().getOrderId() + " has not been started or completed.");
+        }
+    }
 }
