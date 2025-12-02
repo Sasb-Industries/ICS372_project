31ac7be75487af8cc2a0ba8d85e3b4eee11cbb15
sasb-industries <sashkajohnson@gmail.com>
2025-12-01 19:23:37 -0600

    - Added button labels - Added list view - Buttons add items to list view

--- src/main/kotlin/Project2/NewOrderController.kt
+++ src/main/kotlin/Project2/NewOrderController.kt
@@ -2,6 +2,7 @@ package Project2
 
 import javafx.fxml.FXML
 import javafx.scene.control.Button
+import javafx.scene.control.ListView
 
 class NewOrderController {
 
@@ -12,9 +13,34 @@ class NewOrderController {
     @FXML lateinit var itemE: Button
     @FXML lateinit var itemF: Button
 
+    // List for running order tab
+    @FXML lateinit var orderList: ListView<String>
+
     @FXML
     fun initialize() {
-        // later: hook up real actions
-        // for now this is just layout
+
+        itemA.setOnAction {
+            println("Hamburger Selected !")
+            orderList.items.add("Hamburger")}
+
+        itemB.setOnAction {
+            println("French Fries Selected !")
+            orderList.items.add("French Fries")}
+
+        itemC.setOnAction {
+            println("Caesar Salad Selected !")
+            orderList.items.add("Caesar Salad")}
+
+        itemD.setOnAction {
+            println("Fountain Drink Selected !")
+            orderList.items.add("Fountain Drink")}
+
+        itemE.setOnAction {
+            println("Ice Cream Sundae Selected !")
+            orderList.items.add("Ice Cream Sundae")}
+
+        itemF.setOnAction {
+            println("Chicken Sandwich Selected !")
+            orderList.items.add("Chicken Sandwich")}
     }
 }
